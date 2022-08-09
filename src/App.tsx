d485f504b1c162af09de2c899a3c124ffd73a834
minnheima <hertz.minn@gmail.com>
2022-08-09 15:20:19 +0900

    #7.16 Final project part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -10,9 +10,18 @@ const Wrapper = styled(motion.div)`
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
+const Grid = styled.div`
+  width: 50vw;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -20,20 +29,36 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  width: 100px;
-  height: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `;
+
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
-      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
+    <Wrapper>
+      <Grid>
+        {[1, 2, 3, 4].map((n) => (
+          <Box onClick={() => setId(n + "")} key={n} layoutId={n + ""} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ background: "rgba(0, 0, 0, 0)" }}
+            animate={{ background: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ background: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={id} style={{ width: "50vw", height: "30vw" }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
