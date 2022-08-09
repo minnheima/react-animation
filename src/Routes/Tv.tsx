745ba73051caa4b1631a8903a26821d7bff6e958
minnheima <hertz.minn@gmail.com>
2022-08-09 23:33:31 +0900

    #8.2 Header part 2

--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function Tv() {
-  return null;
+  return (
+    <div>
+      <Title>TV show</Title>
+    </div>
+  );
 }
 
 export default Tv;
