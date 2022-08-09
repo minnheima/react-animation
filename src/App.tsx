a72ec0c55ac8816e744f51d492749bf2c45d69bb
minnheima <hertz.minn@gmail.com>
2022-08-09 18:26:33 +0900

    #8.1 Header part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,20 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
-
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
-`;
-const Grid = styled.div`
-  width: 50vw;
-  display: grid;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-const Box = styled(motion.div)`
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const Overlay = styled(motion.div)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-`;
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
 
 function App() {
-  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper>
-      <Grid>
-        {[1, 2, 3, 4].map((n) => (
-          <Box onClick={() => setId(n + "")} key={n} layoutId={n + ""} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ background: "rgba(0, 0, 0, 0)" }}
-            animate={{ background: "rgba(0, 0, 0, 0.5)" }}
-            exit={{ background: "rgba(0, 0, 0, 0)" }}
-          >
-            <Box layoutId={id} style={{ width: "50vw", height: "30vw" }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
