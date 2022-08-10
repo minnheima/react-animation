f024cefa8d55254b612d3f481f450c5b64e52ef7
minnheima <hertz.minn@gmail.com>
2022-08-10 20:20:18 +0900

    #8.7 Slider part1

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -17,7 +19,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 1)), url(${(props) => props.bgPhoto});
   background-size: cover;
@@ -29,20 +31,73 @@ const Title = styled.h2`
 const Overview = styled.p`
   font-size: 28px;
   width: 50%;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
 `;
+
+const Slider = styled.div`
+  position: relative;
+  top: -10vw;
+`;
+const Row = styled(motion.div)`
+  position: absolute;
+  display: grid;
+  width: 100%;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+`;
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 30px;
+`;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
   // console.log(data, isLoading);
+  const [idx, setIdx] = useState(0);
+  const increaseIdx = () => setIdx((prev) => prev + 1);
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner onClick={increaseIdx} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={idx}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((n) => (
+                  <Box key={n}>{n}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
